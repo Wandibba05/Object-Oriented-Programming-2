dd7675842d397782fbb3a655f6e8e49d38c61c9f
Wandibba05 <emmanuel.wandibba@strathmore.edu>
2025-05-05 18:33:02 +0300

    Add files via upload

---
+++ Choices.java
@@ -0,0 +1,14 @@
+public class Choices {
+    public static void main(String[] args) {
+        int Choice = 2;
+
+        if (Choice == 1) {
+            System.out.println("You chose option 1");
+        } else if (Choice == 2) {
+            System.out.println("You chose option 2");
+        } else {
+            System.out.println("Your choice is invalid");
+        }
+    }
+}
+
