e6960c798998eca4888c6acb77eab5314afe97ce
Wandibba05 <emmanuel.wandibba@strathmore.edu>
2025-06-30 14:49:35 +0300

    Add files via upload

---
+++ ArithmeticJava.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class ArithmeticJava {
+    public static void main(String[] args)
+    {
+        int number1 = 100;
+        int number2 = 0;
+        int ans = number1 / number2;
+        System.out.println("Answer: " + ans);
+
+        try{
+            int answer = number1/number2;
+            System.out.println("Answer:" + answer);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Error: Division by zero is not allowed");
+        }
+        finally {
+            System.out.println("Program continues after handling the exception");
+        }
+    }
+}
+
+
